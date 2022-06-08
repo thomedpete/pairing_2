0953222082ec3ba04079a64cd7fa4c0c90eeb1bf
Thomas Peterson <thomasedwardpeterson@gmail.com>
2022-06-08 17:08:08 -0600

    Added some methods

--- friends.js
+++ friends.js
@@ -1,3 +1,8 @@
 var arrays =['bob',"kevin",'sally','prince','pete']
 console.log(arrays[2])
 console.log(arrays[4])
+
+var nuray =['texas','disneyland','hell','denver','dublin']
+
+nuray.pop()
+console.log(nuray)
